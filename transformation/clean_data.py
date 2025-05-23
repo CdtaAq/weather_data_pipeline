e42a7e5ce3068154eb284fea81cea0e15a0f9739
chris Taylor <ctay456a@gmail.com>
2025-05-23 17:28:48 -0400

    Create clean_data.py

---
+++ transformation/clean_data.py
@@ -0,0 +1,7 @@
+def transform_weather_data(raw_data):
+    return {
+        "city": raw_data["name"],
+        "temperature": raw_data["main"]["temp"],
+        "humidity": raw_data["main"]["humidity"],
+        "weather": raw_data["weather"][0]["description"]
+    }
